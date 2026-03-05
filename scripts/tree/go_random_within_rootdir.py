ced5866629c65b49a9ac4dd5fb68528e16d79ffd
Kiyoon Kim <kiyoon@users.noreply.github.com>
2026-03-05 10:28:38 +0900

    .

--- scripts/tree/go_random_within_rootdir.py
+++ scripts/tree/go_random_within_rootdir.py
@@ -10,6 +10,8 @@ lua_code_nvimtree = """
 local main_pane_cwd, side_pane_root = ...
 local nt_api = require('nvim-tree.api')
 
+-- nvim-tree's find_file navigates to the parent directory if the target is
+-- already the root directory, so we early-return to avoid that.
 local initial_root_dir = nt_api.tree.get_nodes().absolute_path
 if initial_root_dir == main_pane_cwd then
   return
